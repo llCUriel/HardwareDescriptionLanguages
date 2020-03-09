c71f83497815df0c4d990df55732d577b09444bb
llCUriel <48939025+llCUriel@users.noreply.github.com>
2020-03-08 23:57:02 -0600

    Commit

---
+++ [2] [Pr] Generalized AdderRipleCarry/AdderRipleCarry.vhd
@@ -0,0 +1,51 @@
+
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.STD_LOGIC_ARITH.ALL;
+USE IEEE.NUMERIC_BIT.ALL;
+USE IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+ENTITY AdderRipleCarry IS
+ 
+	GENERIC (
+		AdderSize : INTEGER := 10
+	);
+ 
+	PORT (
+		A,B : IN  STD_LOGIC_VECTOR(AdderSize - 1 DOWNTO 0);
+		Cin : IN  STD_LOGIC;
+		Cout: OUT STD_LOGIC;
+		Sum : OUT STD_LOGIC_VECTOR(AdderSize - 1 DOWNTO 0)
+	);
+ 
+END AdderRipleCarry;
+
+ARCHITECTURE AdderRipleCarryArchitecture OF AdderRipleCarry IS
+
+SIGNAL Carrys : STD_LOGIC_VECTOR(AdderSize DOWNTO 0);
+	
+COMPONENT FullAdder IS
+	PORT (
+		A,B : IN  STD_LOGIC;
+		Cin : IN  STD_LOGIC;
+		Cout: OUT STD_LOGIC;
+		Sum : OUT STD_LOGIC
+	);
+END COMPONENT;
+
+BEGIN
+	Carrys(0)<=Cin;
+	AdderRipleCarryCycle : FOR I IN 0 TO AdderSize-1 GENERATE
+	FullAdder1 : FullAdder
+		PORT MAP(
+			A => A(I), 
+			B => B(I), 
+			Cin => Carrys(I), 
+			Cout => Carrys(I+1), 
+			Sum => Sum(I)
+		);
+	END GENERATE;
+	Cout<=Carrys(AdderSize);
+
+
+END AdderRipleCarryArchitecture;
